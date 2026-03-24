d489fa436ac0430f7a57bcc51473641a4264f246
oy-hotwater <261808538+oy-hotwater@users.noreply.github.com>
2026-03-24 15:39:18 +0900

    delete any tipe, and update

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,7 +4,15 @@ export interface SvgItem {
   id: string;
   name: string;
   code: string;
-  createdAt: Timestamp | FieldValue | number | null | any;
+  createdAt: Timestamp | FieldValue | number | null;
+}
+
+// UI（コンポーネント）層で扱う加工済みデータの型
+export interface SvgItem {
+  id: string;
+  name: string;
+  code: string;
+  createdAt: Date | null;
 }
 
 export type ViewState = "list" | "add" | "detail";
